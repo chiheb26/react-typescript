647d3ab8292ddddc265a6836d0139c0ff6f29425
chiheb <chiheb.fellah@gmail.com>
2022-07-18 01:30:49 +0100

    wrapping html elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import './App.css';
 import { Private } from './components/auth/Private';
 import { Profile } from './components/auth/Profile';
 import { List } from './components/generics/List';
+import { CustomButton } from './components/html/Button';
 import { RandomNumber } from './components/restrictions/RandomNumber';
 import { Toast } from './components/templateLiterals/Toast';
 
 function App() {
    return (
     <div className="App">
-      <Toast position='Center' />
+      <CustomButton variant='primary'>aa</CustomButton>
     </div>
   );
 }
