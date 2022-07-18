b8751ef6ddde4d177653ea40410f12904ae175b6
chiheb <chiheb.fellah@gmail.com>
2022-07-18 02:08:21 +0100

    polymorphic components

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Private } from './components/auth/Private';
 import { Profile } from './components/auth/Profile';
 import { List } from './components/generics/List';
 import { CustomButton } from './components/html/Button';
+import { Text } from './components/polymorphic/Text';
 import { RandomNumber } from './components/restrictions/RandomNumber';
 import { Toast } from './components/templateLiterals/Toast';
 
 function App() {
    return (
     <div className="App">
-      <CustomButton variant='primary'>aa</CustomButton>
+      <Text as='h1' size='lg'>Heading</Text>
+      <Text as='p' size='md'>Paragraph</Text>
+      <Text as='label' size='sm' color='secondary'>Label</Text>
     </div>
   );
 }
